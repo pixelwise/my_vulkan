84c7e726d817026b7dc50259d94c87f3065ffbca
Daniel Oberhoff <daniel@danieloberhoff.de>
2019-02-18 23:54:40 +0100

    add mutable target rect to render target
    to be used for subrect rendering for screen captures with
    split views

--- my_vulkan/helpers/render_target.hpp
+++ my_vulkan/helpers/render_target.hpp
@@ -15,13 +15,29 @@ namespace my_vulkan
         };
         struct render_target_t
         {
-            std::function<render_scope_t(VkRect2D)> begin;
-            std::function<void(
+            using begin_t = std::function<render_scope_t(VkRect2D)>;
+            using end_t = std::function<void(
                 std::vector<queue_reference_t::wait_semaphore_info_t>,
                 std::vector<VkSemaphore>
-            )> end;
-            VkExtent2D size;
-            size_t depth;
+            )>;
+            const begin_t begin;
+            const end_t end;
+            const VkExtent2D size;
+            const size_t depth;
+            render_target_t(
+                begin_t in_begin,
+                end_t in_end,
+                VkExtent2D in_size,
+                size_t in_depth
+            )
+            : begin{std::move(in_begin)}
+            , end{std::move(in_end)}
+            , size{in_size}
+            , depth{in_depth}
+            , target_rect{{0, 0}, in_size}
+            {
+            }
+            VkRect2D target_rect;
         };
     }
 }
