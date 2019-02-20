bbb0601e034a39b34f7e7922ab3e4a77b746a75d
Daniel Oberhoff <daniel@danieloberhoff.de>
2019-02-20 15:55:01 +0100

    add safe target rect modifier for render target

--- my_vulkan/helpers/render_target.hpp
+++ my_vulkan/helpers/render_target.hpp
@@ -3,6 +3,7 @@
 #include "../command_buffer.hpp"
 #include "../queue.hpp"
 #include <functional>
+#include <stdexcept>
 
 namespace my_vulkan
 {
@@ -25,6 +26,7 @@ namespace my_vulkan
             const VkExtent2D size;
             const size_t depth;
             const bool flipped;
+            const VkRect2D target_rect;
             render_target_t(
                 begin_t in_begin,
                 end_t in_end,
@@ -32,15 +34,51 @@ namespace my_vulkan
                 size_t in_depth,
                 bool in_flipped
             )
+            : render_target_t{
+                in_begin,
+                in_end,
+                in_size,
+                in_depth,
+                in_flipped,
+                {{0, 0}, in_size},
+            }
+            {
+            }
+            render_target_t with_target_rect(VkRect2D rect) const
+            {
+                if (
+                    rect.offset.x < 0 ||
+                    rect.offset.y < 0 ||
+                    rect.extent.width + rect.offset.x > size.width ||
+                    rect.extent.height + rect.offset.y > size.height
+                )
+                    throw std::runtime_error{"target rect not within bounds"};
+                return {
+                    begin,
+                    end,
+                    size,
+                    depth,
+                    flipped,
+                    rect
+                };
+            }
+        private:
+            render_target_t(
+                begin_t in_begin,
+                end_t in_end,
+                VkExtent2D in_size,
+                size_t in_depth,
+                bool in_flipped,
+                VkRect2D in_target_rect
+            )
             : begin{std::move(in_begin)}
             , end{std::move(in_end)}
             , size{in_size}
             , depth{in_depth}
             , flipped{in_flipped}
-            , target_rect{{0, 0}, in_size}
+            , target_rect{in_target_rect}
             {
             }
-            VkRect2D target_rect;
         };
     }
 }
