235e98203175230db6593e42598676e74608357f
Daniel Oberhoff <daniel@danieloberhoff.de>
2021-01-20 14:47:30 +0100

    working on jenkins build

--- jenkins/prepare.sh
+++ jenkins/prepare.sh
@@ -18,5 +18,5 @@ pip3 install ${pip_opts} --upgrade -r prerequirements_dev.txt
 pkgs="$(jinja2 pkglist_dev.txt -D VERSION_ID='$(lsb_release -r -s)' | tr '\n' ' ')"
 # we do not want the source and jinja2 run with sudo
 sudo dnf -y install ${pkgs}
-pip3 install --upgrade --user -r requirements_dev.txt
+pip3 install --upgrade -r requirements_dev.txt
 git clean -xdf
