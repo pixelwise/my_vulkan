b9599042d15c65a371ff921e46827e4f799d0ab4
Daniel Oberhoff <daniel@danieloberhoff.de>
2021-01-20 14:28:01 +0100

    try to fix jenkins build

--- jenkins/prepare.sh
+++ jenkins/prepare.sh
@@ -1,7 +1,22 @@
-
 set -e
-pip3 install --upgrade --user -r requirements_dev.txt
+env_paths=(
+"/opt/gameon/gdenv"
+)
+for env_path in "${env_paths[@]}"
+do
+    if [ -e "${env_path}" ]
+    then
+        echo "source ${env_path}"
+        source "${env_path}"
+    fi
+done
+if [ -z "${VIRTUAL_ENV}" ]
+then
+    pip_opts="--user"
+fi
+pip3 install ${pip_opts} --upgrade -r prerequirements_dev.txt
 pkgs="$(jinja2 pkglist_dev.txt -D VERSION_ID='$(lsb_release -r -s)' | tr '\n' ' ')"
 # we do not want the source and jinja2 run with sudo
 sudo dnf -y install ${pkgs}
+pip3 install --upgrade --user -r requirements_dev.txt
 git clean -xdf
